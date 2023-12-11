b4c20b50e85b5a898b2d7e701495b7053a5a0cc2
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 21:12:32 -0800

    updated routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,12 +4,15 @@ const PORT = 3001;
 const app = express();
 
 // GET route
+app.get('/notes', (req, res) => {
+    res.send(notes.html)
+})
+
 app.get('/', (req, res) => {
-    res.send('notes')
-  })
-  
-  // POST route
-  app.post('/', (req, res) => {
-    res.send('notes')
-  })
+    res.send(index.html)
+})
+// POST route
+app.post('/', (req, res) => {
+    res.send('Post request to the homepage')
+})
 
